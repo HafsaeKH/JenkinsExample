874659f7d4fe5f0eb225ce8508a06ffc21371cd3
HafsaeKH <hafsae.hk@gmail.com>
2019-09-28 18:44:57 +0100

    commit

---
+++ src/main/java/org/mql/Jenkins/example1/p01_Jenkins/Test1.java
@@ -0,0 +1,10 @@
+package org.mql.Jenkins.example1.p01_Jenkins;
+
+
+public class Test1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jenkins !!" );
+    }
+}
